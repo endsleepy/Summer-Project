6f6180312e7f7839e943a73b7562416a0e372af6
Sleepyy9 <mightybj10@gmail.com>
2025-07-08 19:14:33 -0400

    tiny change, no functional impact
    Little to no changes.

--- app.py
+++ app.py
@@ -4,27 +4,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_templates('index.html')
+    return render_template('index.html')
 
 @app.route('/registration')
 def registration():
-    return render_templates('register.html')
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/timetable')
 def timetable():
-    return render_templates('timetable.html')
+    return render_template('timetable.html')
 
 @app.route('/timetable/days')
 def timetable_days():
-    return render_templates('timetable_day.html')
+    return render_template('timetable_day.html')
 
 @app.route('/timetable/grades')
 def timetable_grades():
-    return render_templates('timetable_grade.html')
+    return render_template('timetable_grade.html')
 
 @app.route('/timetable/coaches')
 def timetable_coaches():
-    return render_templates('timetable_coach.html')
+    return render_template('timetable_coach.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
